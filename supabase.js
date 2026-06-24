5322a1ba14adf6e20d5bf69e28d5748eca68a5e9
TevinG25 <bida24-151@thuto.bac.ac.bw>
2026-06-24 20:43:43 +0200

    Initialize Supabase client in supabase.js

---
+++ supabase.js
@@ -0,0 +1,6 @@
+const { createClient } = supabase
+
+const db = createClient(
+  'https://ccoylddolnrpywvjycur.supabase.co',
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNjb3lsZGRvbG5ycHl3dmp5Y3VyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODE4MTc0NDYsImV4cCI6MjA5NzM5MzQ0Nn0.ZYaOZKKue6H5Wxr68KH9UZhgalOaBEsS8Ju8i4BriRM'
+)
